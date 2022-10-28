a761d515b8991858afe1e9f2f7ea0a8c46f50bb9
Pinocchio <officialnikx@gmail.com>
2022-10-28 07:12:33 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=> {
+  res.status.json({
+    status: 'success',
+    data: {
+      slackUsername: 'Pinocchio',
+      backend: true,
+      age: 23,
+      bio: 'i am a boy trying to make it'
+    }
+  })
+})
+app.listen(PORT,()=>{
+  console.log(`serving on port ${PORT}`)
+})
